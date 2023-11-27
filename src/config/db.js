4eb1e2d4b9771b603ae3ab3ca9ab8201f43227c3
valeriia zvezdina <valeriia.zvezdina@gmail.com>
2023-11-27 12:25:12 +0100

    added host

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,8 @@ const sequelize = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-        dialect: 'postgres'
+      host: process.env.DB_HOST,
+      dialect: 'postgres'
     }
 );
 
