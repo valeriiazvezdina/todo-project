794b4f3ab6bbae42cae89e85d86c2381c5470e43
valeriia zvezdina <valeriia.zvezdina@gmail.com>
2023-11-27 12:37:49 +0100

    sync relations

--- src/models/models.js
+++ src/models/models.js
@@ -2,22 +2,22 @@ const userModel = require('./user.model');
 const todoModel = require('./todo.model');
 
 // /* Creating the model */
-// userModel.sync()
-//                 .then(() => {
-//                     console.log('User model was created successfully');
-//                 })
-//                 .catch(err => {
-//                     console.log(err.message);
-//                 });
+userModel.sync()
+                .then(() => {
+                    console.log('User model was created successfully');
+                })
+                .catch(err => {
+                    console.log(err.message);
+                });
 
 // /* Creating the model */
-// todoModel.sync()
-//                 .then(() => {
-//                     console.log('Todo model was created successfully');
-//                 })
-//                 .catch(err => {
-//                     console.log(err.message);
-//                 });
+todoModel.sync()
+                .then(() => {
+                    console.log('Todo model was created successfully');
+                })
+                .catch(err => {
+                    console.log(err.message);
+                });
 
 /* Setting up the relations */
 userModel.hasMany(todoModel, { foreignKey: 'idUser' });
